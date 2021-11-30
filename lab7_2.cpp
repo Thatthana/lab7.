565ba01072fe69024d0c39b2b101c174c5e3e1d7
Fluke <hunterx52110@gmail.com>
2021-11-30 16:38:56 +0700

    lab7_2 completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,10 +4,28 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
+	int i = 0, P[5000], k, result=0;
+	while(i < 5000) {
 		P[i] = 2*i+1;
 		i++;
+	}
+
+	cout << "Please input k: ";
+	cin >> k;
+	
+	if (k > 0){ 
+		i=0;
+		while (i < 5000){
+			if (P[i]%k == 0){
+				result = result - P[i];
+			}else{
+				result = result + P[i];
+			}
+			i++;
+		}
+		cout << "Result = " << result << endl;
+	}else{
+		cout << "Invaild input!!!" << endl;
 	}
 	
 	return 0;
